064185af9463175e8add04a867d01feba61e07f8
m1xael <karpovmichael@outlook.com>
2024-05-04 00:38:29 +0200

    Dashboard initial implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//Sets up the main structure of an application with routing for different pages.
 
+//Imports
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+
+
+// Wrapping the entire application in the AuthProvider to make authentication context available throughout the app.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+        </Routes>
+      </Router>
   );
 }
 
