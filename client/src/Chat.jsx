e6592912dbf6dd948914aa37cfca9e7150596e5d
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2024-05-04 19:32:21 +0200

    fix first message appearing

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,21 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Box, Typography, TextField, Button } from '@mui/material';
 
+// TODO
+// first message not showing up
+// message order
+// loading state
+// formatting the incoming message
+// UI polish
+
 function Chat() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
-  const bottomRef = useRef(null); // Reference to help with autoscroll
+  const [loading, setLoading] = useState(false);
+  // const bottomRef = useRef(null); // Reference to help with autoscroll
 
-  const sendMessage = (event) => {
+  const sendMessage = async (event) => {
     event.preventDefault();
     const trimmedInput = input.replace(/\s+/g, '');
     if (trimmedInput) {
       setMessages(prevMessages => [...prevMessages, { text: input, sender: 'user' }]);
       setInput(''); 
   
-      mockBackendRequest(trimmedInput).then(response => {
-        setMessages(prevMessages => [...prevMessages, { text: response, sender: 'bot' }]);
-      });
+      await backendRequest(trimmedInput);
     }
   };
   
@@ -23,39 +29,57 @@ function Chat() {
     setMessages([]); 
   };
 
-  const mockBackendRequest = async (userInput) => {
-    return new Promise(resolve => setTimeout(() => resolve(`Received: ${userInput}`), 500));
+  const backendRequest = async (userInput) => {
+    const baseUrl = 'http://localhost:8000/chat/advisor/'
+    const queryParams = { query: userInput };
+    const queryString = new URLSearchParams(queryParams).toString();
+    const url = `${baseUrl}?${queryString}`;
+
+    try {
+        setLoading(true)
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Something went wrong.');
+        }
+        const data = await response.json();
+        setMessages(prevMessages => [...prevMessages, { text: data.response, sender: 'bot' }]);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    } finally {
+        setLoading(false)
+    }
   };
 
-  useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
+  // useEffect(() => {
+  //   bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  // }, [messages]);
 
   return (
     <div style={{ position: 'fixed', width: '100%', height: '100%', top: 0, left: 0, background: '#f0f2f5' }}>
       <Box sx={{
         position: 'fixed',
         top: 0,
-        bottom: '3em', 
+        bottom: '7em',
         left: 0,
         right: 0,
-        overflowY: 'auto', 
-        padding: '1em',
+        overflowY: 'auto',
+        padding: '2em',
         display: 'flex',
         flexDirection: 'column-reverse',
       }}>
         {messages.map((message, index) => (
           <Typography key={index} sx={{
             alignSelf: message.sender === 'user' ? 'flex-end' : 'flex-start',
-            backgroundColor: message.sender === 'user' ? '#AEDFF7' : '#D3D3D3', 
+            backgroundColor: message.sender === 'user' ? '#AEDFF7' : '#D3D3D3',
             padding: '0.5em',
             borderRadius: '10px',
             margin: '0.5em',
+            maxWidth: '45%'
           }}>
             {message.text}
           </Typography>
         ))}
-        <div ref={bottomRef} />
+        {/*<div ref={bottomRef} />*/}
       </Box>
       <Box sx={{
         position: 'fixed',
@@ -63,7 +87,7 @@ function Chat() {
         left: 0,
         right: 0,
         padding: '1em',
-        backgroundColor: '#ffff', 
+        backgroundColor: '#ffff',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
